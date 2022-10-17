00ac33337bdad3f2eb63a6a08b15011f96dd228d
hoangtnhe130353 <d3cia.tnhoang@gmail.com>
2022-10-17 15:46:58 +0700

    Update web
    Update web

---
+++ web/client-static/_next/static/chunks/pages/admin-d4509059a8f591bf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[964],{4830:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin",function(){return t(6527)}])},6527:function(n,u,t){"use strict";t.r(u);var i=t(5893),_=function(){return(0,i.jsx)(i.Fragment,{children:"Admin"})};u.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4830)}),_N_E=n.O()}]);
