b557d440774ad4197a26cdf2364c3b5b6bbff874
Hong Phuc <phucvh18@gmail.com>
2022-10-18 16:08:41 +0700

    AddNewProduct

--- src/main/java/com/swp/sbeauty/service/impl/ProductServiceImpl.java
+++ src/main/java/com/swp/sbeauty/service/impl/ProductServiceImpl.java
@@ -1,10 +1,7 @@
 package com.swp.sbeauty.service.impl;
 
 import com.swp.sbeauty.dto.ProductDto;
-import com.swp.sbeauty.dto.UserDto;
-import com.swp.sbeauty.entity.Product;
-import com.swp.sbeauty.entity.Role;
-import com.swp.sbeauty.entity.User;
+import com.swp.sbeauty.entity.*;
 import com.swp.sbeauty.repository.CategoryRepository;
 import com.swp.sbeauty.repository.ProductRepository;
 import com.swp.sbeauty.repository.SupplierRepository;
@@ -14,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -38,6 +36,36 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDto saveProduct(ProductDto productDto) {
+
+        if(productDto != null){
+            Product product = new Product();
+            product.setName(productDto.getName());
+            product.setImportPrice(productDto.getImportPrice());
+            product.setSalePrice(productDto.getSalePrice());
+            product.setEXP((Date) productDto.getEXP());
+            product.setMFG((Date) productDto.getMFG());
+            product.setDescription(product.getDescription());
+            if(productDto.getSupplier()!=null){
+                Supplier supplier = null;
+                Optional<Supplier> optional = supplierRepository.findById(productDto.getSupplier().getId());
+                if (optional.isPresent()) {
+                    supplier = optional.get();
+                }
+                product.setSupplier(supplier);
+            }
+            if(productDto.getCategory()!=null){
+                Category category = null;
+                Optional<Category> optional = categoryRepository.findById(productDto.getCategory().getId());
+                if (optional.isPresent()) {
+                    category = optional.get();
+                }
+                product.setCategory(category);
+            }
+            product = productRepository.save(product);
+            if(product != null){
+                return new ProductDto(product);
+            }
+        }
         return null;
     }
 
