1f5a303d6e318423a450ac50151a629048486452
manhpdhe140831 <manhpdhe140831@fpt.edu.vn>
2022-10-26 16:27:54 +0700

    update jwt

--- src/main/java/com/swp/sbeauty/controller/UserController.java
+++ src/main/java/com/swp/sbeauty/controller/UserController.java
@@ -82,11 +82,7 @@ public class UserController {
         String jwt = jwtUtils.generateJwtToken(authentication);
 
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(jwt);
+        return ResponseEntity.ok(new JwtResponse(jwt));
     }
     //abc
 //
