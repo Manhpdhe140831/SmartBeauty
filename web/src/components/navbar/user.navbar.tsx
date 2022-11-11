82b4a4201183a193f7fe1c9b6aa0eb234444d085
hoangtnhe130353 <69812397+hoangtnhe130353@users.noreply.github.com>
2022-11-11 23:44:09 +0700

    Fixing HTML

--- web/src/components/navbar/user.navbar.tsx
+++ web/src/components/navbar/user.navbar.tsx
@@ -1,6 +1,5 @@
 import { Avatar, NavLink, Text, Tooltip, useMantineTheme } from "@mantine/core";
 import { IconChevronRight } from "@tabler/icons";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useAuthUser } from "../../store/auth-user.state";
 
@@ -14,32 +13,31 @@ const UserNavbar = () => {
   }
 
   return (
-    <Link href={"/profile"} passHref>
-      <NavLink
-        component={"a"}
-        active={router.pathname.startsWith("/profile")}
-        icon={<Avatar src={user.image} radius="xl" />}
-        label={
-          <Tooltip label={user.email}>
-            <Text size="sm" weight={500}>
-              {user.name}
-            </Text>
-          </Tooltip>
-        }
-        description={
-          <Text color="dimmed" className={"uppercase"} size="xs">
-            {user.role}
+    <NavLink
+      href={"/profile"}
+      component={"a"}
+      active={router.pathname.startsWith("/profile")}
+      icon={<Avatar src={user.image} radius="xl" />}
+      label={
+        <Tooltip label={user.email}>
+          <Text size="sm" weight={500}>
+            {user.name}
           </Text>
-        }
-        rightSection={<IconChevronRight size={18} />}
-        className="hover:!bg-gray-700"
-        sx={{
-          padding: theme.spacing.xs,
-          borderRadius: theme.radius.sm,
-          color: theme.colors.dark[0],
-        }}
-      ></NavLink>
-    </Link>
+        </Tooltip>
+      }
+      description={
+        <Text color="dimmed" className={"uppercase"} size="xs">
+          {user.role}
+        </Text>
+      }
+      rightSection={<IconChevronRight size={18} />}
+      className="hover:!bg-gray-700"
+      sx={{
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color: theme.colors.dark[0],
+      }}
+    ></NavLink>
   );
 };
 
