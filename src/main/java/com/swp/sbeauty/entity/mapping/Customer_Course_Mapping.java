c0517cb1c1913b0339cb2df6765502ba491b71ca
Hong Phuc <phucvh18@gmail.com>
2022-11-12 11:29:37 +0700

    fix serviece buyed

--- src/main/java/com/swp/sbeauty/entity/mapping/Customer_Course_Mapping.java
+++ src/main/java/com/swp/sbeauty/entity/mapping/Customer_Course_Mapping.java
@@ -1,10 +1,17 @@
 package com.swp.sbeauty.entity.mapping;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
 public class Customer_Course_Mapping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
