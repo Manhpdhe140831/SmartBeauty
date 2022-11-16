12e89991fb0e3865e4b1dd0f65aca862f1ec0217
Hong Phuc <phucvh18@gmail.com>
2022-11-16 13:58:19 +0700

    fix

--- src/main/java/com/swp/sbeauty/repository/mappingRepo/Bill_Course_History_Repository.java
+++ src/main/java/com/swp/sbeauty/repository/mappingRepo/Bill_Course_History_Repository.java
@@ -10,15 +10,17 @@ import java.util.List;
 
 @Repository
 public interface Bill_Course_History_Repository extends JpaRepository<Bill_Course_History, Long> {
-    @Query(value = "select co.* from bill_course_history co, customer_course_mapping cc, bill b, bill_customer_mapping bc, customer c, customer_branch_mapping cb\n" +
-            "            where co.id = cc.course_id\n" +
-            "            and cc.bill_id = b.id\n" +
-            "            and b.id = bc.bill_id\n" +
-            "            and bc.customer_id = c.id\n" +
-            "            and c.id = cb.id_customer\n" +
-            "            and cb.id_branch =?1\n" +
-            "            and c.id =?2\n" +
-            "            and b.status IN ('dathanhtoan')\n" +
-            "            and cc.status IN ('chuasudung', 'dangsudung')", nativeQuery = true)
+    @Query(value = "select s.* from bill_course_history s, bill_detail bd, customer_course_mapping ccm,bill_bill_detail_mapping bbd ,bill b, bill_customer_mapping bcm, customer c,customer_branch_mapping cb\n" +
+            "where bd.id = s.bill_detail_id\n" +
+            "and ccm.bill_detail_id = bd.id\n" +
+            "and bd.id = bbd.bill_detail_id\n" +
+            "and bbd.bill_id =b.id\n" +
+            "and b.id = bcm.bill_id\n" +
+            "and bcm.customer_id = c.id\n" +
+            "and c.id = cb.id_customer\n" +
+            "and cb.id_branch =?1\n" +
+            "and c.id =?2\n" +
+            "and b.status IN('dathanhtoan')\n" +
+            "and ccm.status IN('chuasudung','dangsudung')", nativeQuery = true)
     List<Bill_Course_History> getCourseBuyed(Long idBranch, Long idCustomer);
 }
--- src/main/java/com/swp/sbeauty/repository/mappingRepo/Bill_Service_History_Repository.java
+++ src/main/java/com/swp/sbeauty/repository/mappingRepo/Bill_Service_History_Repository.java
@@ -9,15 +9,17 @@ import java.util.List;
 
 @Repository
 public interface Bill_Service_History_Repository extends JpaRepository<Bill_Service_History, Long> {
-    @Query(value = "select s.* from bill_service_history s, customer_course_mapping cc, bill b, bill_customer_mapping bc, customer c , customer_branch_mapping cb\n" +
-            "where s.id = cc.service_id\n" +
-            "and cc.bill_id = b.id\n" +
-            "and b.id = bc.bill_id\n" +
-            "and bc.customer_id = c.id\n" +
+    @Query(value = "select s.* from bill_service_history s, bill_detail bd, customer_course_mapping ccm,bill_bill_detail_mapping bbd ,bill b, bill_customer_mapping bcm, customer c,customer_branch_mapping cb\n" +
+            "where bd.id = s.bill_detail_id\n" +
+            "and ccm.bill_detail_id = bd.id\n" +
+            "and bd.id = bbd.bill_detail_id\n" +
+            "and bbd.bill_id =b.id\n" +
+            "and b.id = bcm.bill_id\n" +
+            "and bcm.customer_id = c.id\n" +
             "and c.id = cb.id_customer\n" +
-            "and cb.id = ?1\n" +
+            "and cb.id_branch =?1\n" +
             "and c.id =?2\n" +
             "and b.status IN('dathanhtoan')\n" +
-            "and cc.status IN('chuasudung')", nativeQuery = true)
+            "and ccm.status IN('chuasudung')", nativeQuery = true)
     List<Bill_Service_History> getAllServiceBuyed(Long idBranch, Long idCustomer);
 }
