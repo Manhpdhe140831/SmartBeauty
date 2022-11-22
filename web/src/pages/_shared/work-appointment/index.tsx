49e8533d94e064e5f7db4c592182b678841bcd0b
vuhailam2000 <110183432+mayybe061020@users.noreply.github.com>
2022-11-22 08:07:11 +0700

    Fix change mock slot data

--- web/src/pages/_shared/work-appointment/index.tsx
+++ web/src/pages/_shared/work-appointment/index.tsx
@@ -1,17 +1,17 @@
-import { Button, Text } from "@mantine/core";
+import {Button, Text} from "@mantine/core";
 import CalendarController from "./_partial/calendar-controller";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import {useQuery} from "@tanstack/react-query";
+import {useState} from "react";
 import AppointmentTimeline from "../../../components/appointment-timeline/appointment.timeline";
-import { slotWorkConst } from "../../../mock/slot-work.const";
+import {slotWork} from "../../../mock/slot-work.const";
 import AppointmentHeaderTimeline from "../../../components/appointment-timeline/appointment-header.timeline";
 import mockSchedule from "../../../mock/schedule";
-import { AppPageInterface } from "../../../interfaces/app-page.interface";
-import { useRouter } from "next/router";
-import { USER_ROLE } from "../../../const/user-role.const";
+import {AppPageInterface} from "../../../interfaces/app-page.interface";
+import {useRouter} from "next/router";
+import {USER_ROLE} from "../../../const/user-role.const";
 
 type workAppointmentProps = {
-  userRole: USER_ROLE;
+    userRole: USER_ROLE;
 };
 
 /**
@@ -21,66 +21,66 @@ type workAppointmentProps = {
  */
 
 const WorkAppointment: AppPageInterface<workAppointmentProps> = ({
-  userRole,
-}) => {
-  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+                                                                     userRole,
+                                                                 }) => {
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
-  const { data: allSchedule } = useQuery(
-    ["list-schedule", selectedDate],
-    async () => {
-      console.log(selectedDate);
-      // todo: Tìm kiếm API theo ngày
-      const schedule = await mockSchedule();
-      return schedule;
-    }
-  );
+    const {data: allSchedule} = useQuery(
+        ["list-schedule", selectedDate],
+        async () => {
+            console.log(selectedDate);
+            // todo: Tìm kiếm API theo ngày
+            const schedule = await mockSchedule();
+            return schedule;
+        }
+    );
 
-  const router = useRouter();
+    const router = useRouter();
 
-  const goToBooking = () => {
-    void router.push({
-      pathname: "/manager/manage-schedule/work-appointment/manage-booking",
-    });
-  };
+    const goToBooking = () => {
+        void router.push({
+            pathname: "/manager/manage-schedule/work-appointment/manage-booking",
+        });
+    };
 
-  return (
-    <div className={"flex h-full flex-col space-y-4"}>
-      {/*<Divider my={8} mx={8} />*/}
-      <div className="flex-1 pl-4">
-        <div className="flex h-24 flex-row items-center justify-between border-b">
-          {userRole === USER_ROLE.sale_staff && (
-            <Button onClick={goToBooking}>Đặt lịch</Button>
-          )}
-          <CalendarController
-            dateData={selectedDate}
-            onChange={setSelectedDate}
-          />
-        </div>
-        <div className="flex flex-col">
-          <div className={"flex w-full flex-row"}>
-            <div className="h-16 min-w-32 max-w-32 select-none border-x p-4 font-semibold">
-              <Text className={"uppercase"}>Bed Number</Text>
+    return (
+        <div className={"flex h-full flex-col space-y-4"}>
+            {/*<Divider my={8} mx={8} />*/}
+            <div className="flex-1 pl-4">
+                <div className="flex h-24 flex-row items-center justify-between border-b">
+                    {userRole === USER_ROLE.sale_staff && (
+                        <Button onClick={goToBooking}>Đặt lịch</Button>
+                    )}
+                    <CalendarController
+                        dateData={selectedDate}
+                        onChange={setSelectedDate}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <div className={"flex w-full flex-row"}>
+                        <div className="h-16 min-w-32 max-w-32 select-none border-x p-4 font-semibold">
+                            <Text className={"uppercase"}>Bed Number</Text>
+                        </div>
+                        <AppointmentTimeline/>
+                    </div>
+                    {slotWork &&
+                        slotWork.map((slot, i) => {
+                            return (
+                                <AppointmentHeaderTimeline
+                                    key={slot.id}
+                                    userRole={userRole}
+                                    title={slot.name}
+                                    timeFrame={slot.timeline}
+                                    bedSchedule={allSchedule?.find(
+                                        (s) => s.slot === slot.id
+                                    )}
+                                />
+                            );
+                        })}
+                </div>
             </div>
-            <AppointmentTimeline />
-          </div>
-          {slotWorkConst &&
-            Object.keys(slotWorkConst).map((slot: any, i) => {
-              return (
-                <AppointmentHeaderTimeline
-                  key={slotWorkConst[slot].id}
-                  userRole={userRole}
-                  title={slotWorkConst[slot].name}
-                  timeFrame={slotWorkConst[slot].timeline}
-                  bedSchedule={allSchedule?.find(
-                    (s) => s.slot === slotWorkConst[slot].id
-                  )}
-                />
-              );
-            })}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default WorkAppointment;
