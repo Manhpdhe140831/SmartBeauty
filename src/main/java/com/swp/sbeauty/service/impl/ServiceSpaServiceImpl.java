b17ffb6a935d0377ce30128f42dc9cc1773060c9
manhpdhe140831 <manhpdhe140831@fpt.edu.vn>
2022-11-23 10:08:33 +0700

    get course service

--- src/main/java/com/swp/sbeauty/service/impl/ServiceSpaServiceImpl.java
+++ src/main/java/com/swp/sbeauty/service/impl/ServiceSpaServiceImpl.java
@@ -213,12 +213,11 @@ public class ServiceSpaServiceImpl implements ServiceSpaService {
         }
         if(ccm != null){
             Bill_Course_History history = bill_course_history_repository.getBill_Course_HistoriesById(ccm.getCourse_id());
-            CourseDto courseDto = new CourseDto(ccm.getId(), history.getName(), history.getPrice(),history.getDuration(),history.getTimeOfUse(),history.getDiscountStart(),history.getDiscountEnd(),history.getDiscountPercent(),history.getImage(),"using",history.getDescription());
+            CourseDto courseDto = new CourseDto(history.getId(), history.getName(), history.getPrice(),history.getDuration(),history.getTimeOfUse(),history.getDiscountStart(),history.getDiscountEnd(),history.getDiscountPercent(),history.getImage(),"buyed",history.getDescription());
             List<Course> courses = courseRepository.getCourseExpelId(history.getCourse_id(),keyword);
             courseDtos.add(courseDto);
             for (Course course : courses){
                 courseDtos.add(new CourseDto("not yet",course));
-
             }
         }
         List<Service> services = serviceRepository.findService(keyword);
