f2cb96028c18830bc83f371020011a47d4e0a90f
hoangtnhe130353 <69812397+hoangtnhe130353@users.noreply.github.com>
2022-11-24 23:50:26 +0700

    update

--- web/src/services/schedule.service.ts
+++ web/src/services/schedule.service.ts
@@ -1,57 +1,74 @@
-import axios, {AxiosError} from "axios";
-import {IErrorResponse,} from "../interfaces/api.interface";
+import axios, { AxiosError } from "axios";
+import { IErrorResponse } from "../interfaces/api.interface";
 
 export async function getSchedule(date: string) {
-    try {
-        const apiResult = await axios.get(`/schedule?date=${date}`);
-        return apiResult.data;
-    } catch (e) {
-        const error = e as AxiosError<IErrorResponse>;
-        console.error(error);
-        throw error.response?.data;
-    }
+  try {
+    const apiResult = await axios.get(`/schedule`, {
+      params: {
+        date,
+      },
+    });
+    return apiResult.data;
+  } catch (e) {
+    const error = e as AxiosError<IErrorResponse>;
+    console.error(error);
+    throw error.response?.data;
+  }
 }
 
 export async function getSlot() {
-    try {
-        const apiResult = await axios.get("/slot");
-        return apiResult.data;
-    } catch (e) {
-        const error = e as AxiosError<IErrorResponse>;
-        console.error(error);
-        throw error.response?.data;
-    }
+  try {
+    const apiResult = await axios.get("/slot");
+    return apiResult.data;
+  } catch (e) {
+    const error = e as AxiosError<IErrorResponse>;
+    console.error(error);
+    throw error.response?.data;
+  }
 }
 
-export async function getServicesAndCourse(idCustomer: number, keyword: string) {
-    try {
-        const apiResult = await axios.get(`/service/findServiceCourse?idCustomer=${idCustomer}&keyword=${keyword}`);
-        return apiResult.data;
-    } catch (e) {
-        const error = e as AxiosError<IErrorResponse>;
-        console.error(error);
-        throw error.response?.data;
-    }
+export async function getServicesAndCourse(
+  idCustomer: number,
+  keyword: string
+) {
+  try {
+    const apiResult = await axios.get(`/service/findServiceCourse`, {
+      params: {
+        idCustomer,
+        keyword,
+      },
+    });
+    return apiResult.data;
+  } catch (e) {
+    const error = e as AxiosError<IErrorResponse>;
+    console.error(error);
+    throw error.response?.data;
+  }
 }
 
 export async function getBedAndStaff(date: string, slotId: number) {
-    try {
-        const apiResult = await axios.get(`/bed/getStaffAndBedFree?date=${date}&slot=${slotId}`);
-        return apiResult.data;
-    } catch (e) {
-        const error = e as AxiosError<IErrorResponse>;
-        console.error(error);
-        throw error.response?.data;
-    }
+  try {
+    const apiResult = await axios.get(`/bed/getStaffAndBedFree`, {
+      params: {
+        date,
+        slotId,
+      },
+    });
+    return apiResult.data;
+  } catch (e) {
+    const error = e as AxiosError<IErrorResponse>;
+    console.error(error);
+    throw error.response?.data;
+  }
 }
 
 export async function createSchedule(payload: any) {
-    try {
-        const apiResult = await axios.post(`/schedule/create`, payload);
-        return apiResult.data;
-    } catch (e) {
-        const error = e as AxiosError<IErrorResponse>;
-        console.error(error);
-        throw error.response?.data;
-    }
+  try {
+    const apiResult = await axios.post(`/schedule/create`, payload);
+    return apiResult.data;
+  } catch (e) {
+    const error = e as AxiosError<IErrorResponse>;
+    console.error(error);
+    throw error.response?.data;
+  }
 }
