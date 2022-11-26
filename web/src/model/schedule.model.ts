8acdd5604948c90971ecb2e650b99b0b32396c1f
hoangtnhe130353 <69812397+hoangtnhe130353@users.noreply.github.com>
2022-11-27 02:13:21 +0700

    Update schedule.model.ts

--- web/src/model/schedule.model.ts
+++ web/src/model/schedule.model.ts
@@ -1,24 +1,23 @@
-import {StaffModel} from "./staff.model";
-import {UserModel} from "./user.model";
-import {SpaBedModel} from "./spa-bed.model";
-import {ServiceModel} from "./service.model";
+import { StaffModel } from "./staff.model";
+import { UserModel } from "./user.model";
+import { SpaBedModel } from "./spa-bed.model";
+import { ServiceModel } from "./service.model";
 
 export interface BookingModel {
-    id: number;
-    slot: number;
-    bed: SpaBedModel | undefined;
-    sale_staff: StaffModel | undefined;
-    tech_staff: StaffModel | undefined;
-    customer: UserModel | undefined;
-    service?: ServiceModel | undefined;
-    course?: ServiceModel | undefined;
-    status: string;
-    note: string;
+  id: number;
+  slot: number;
+  bed: SpaBedModel | undefined;
+  sale_staff: StaffModel | undefined;
+  tech_staff: StaffModel | undefined;
+  customer: UserModel | undefined;
+  service?: ServiceModel | undefined;
+  course?: ServiceModel | undefined;
+  status: string;
+  note: string;
 }
 
 export enum ScheduleStatus {
-    Cancel,
-    Waiting,
-    // OnGoing,
-    Finish,
+  Cancel,
+  Waiting,
+  Finish,
 }
